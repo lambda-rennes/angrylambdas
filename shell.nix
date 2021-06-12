f07410259a51b58af1cd87cc150e43e0d3d84585
Gaël Deest <gael.deest@tweag.io>
2021-06-12 10:21:31 +0200

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,30 @@
+# with (import ./nix {});
+with (import <nixpkgs> {});
+# let srcs = import ./nix/sources.nix;
+#    ghcide-nix = import srcs.ghcide-nix {};
+# in
+
+stdenv.mkDerivation {
+  name = "build-shell";
+  buildInputs = [
+    cabal-install
+    # mesa_drivers
+    # mesa_noglu.drivers
+    libGL
+    # mesa_glu
+    libGLU
+    # libGL_driver
+    glxinfo
+    git
+    freeglut
+    zlib
+    haskell.compiler.ghc8101
+    # ghcide-nix.ghcide-ghc865
+    # vscode
+  ];
+  shellHook = ''
+    export PATH="$PATH:$PWD/bin"
+  '';
+  # I wish we could do without this ugly hack.
+  LD_LIBRARY_PATH = with pkgs; "${libGL}/lib:${mesa_glu}/lib:${freeglut}/lib";
+}
