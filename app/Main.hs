4e5685bd0aff32b9bba58cdacaf7ee7bc513e87c
Gaël Deest <gael.deest@tweag.io>
2021-06-19 21:58:09 +0200

    Moving out collision logic

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,10 @@
 
 module Main where
 
+import Assets (Assets (..))
+import qualified Assets
 import Chiphunk.Low
+import Constants
 import Control.Concurrent.STM (STM)
 import qualified Control.Concurrent.STM as STM
 import Control.Concurrent.STM.TQueue (TQueue)
@@ -16,17 +19,13 @@ import GHC.Float
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Interface.IO.Game
-import System.Exit
-
-import Rendering
-import World
-import Utils
 import Physics
+import Rendering
+import System.Exit
+import Utils
+import World
 
-import Assets (Assets(..))
-import qualified Assets
-import Constants
-
+import Collisions
 
 main :: IO ()
 main = do
@@ -36,54 +35,36 @@ main = do
   collisionQueue <- STM.atomically TQueue.newTQueue
   assets <- Assets.load
 
-  world <- createWorld assets space 
+  world <- createWorld assets space
   createCallBacks space collisionQueue
-
 
   playIO window black 60 world (render assets) (handleEvent assets) (advanceSim space collisionQueue advanceWorld)
 
-
-
-
 window :: Display
-window = FullScreen 
--- window = InWindow "Abstract them all" (2000, 1000) (500, 500)
+window = FullScreen
 
+-- window = InWindow "Abstract them all" (2000, 1000) (500, 500)
 
 ballInitPos :: Pos
 ballInitPos = initSlingshotPos
-
-collisionCallback :: CollisionCallback Bool
-collisionCallback arbiter space _ = do
-  putStrLn "COLLISION !"
-  pure True
-
-enemyCollisionCallback :: TQueue EnemyCollision -> CollisionCallback ()
-enemyCollisionCallback collisionQueue arbiter space _ = do
-  (enemyCollisionBody, _) <- get $ arbiterBodies arbiter
-  enemyCollisionTotalImpulse <- get $ arbiterTotalImpulse arbiter
-  STM.atomically $ TQueue.writeTQueue collisionQueue $ EnemyCollision {..}
 
 
 initBall :: Slingshot
 initBall = Slingshot ballRadius initSlingshotPos Free
 
-createBlock' :: Space -> Picture -> BoxInfo Float -> Pos -> IO Block' 
+createBlock' :: Space -> Picture -> BoxInfo Float -> Pos -> IO Block'
 createBlock' space blockImg boxInfo pos = do
   blockBody <- createBox space boxInfo pos
-  pure $ Block' { blockPicture = blockImg
-               , blockBody = blockBody 
-               }
+  pure $
+    Block'
+      { blockPicture = blockImg,
+        blockBody = blockBody
+      }
 
 createBall :: Space -> Picture -> DiscInfo Float -> Pos -> (Float, Float) -> IO Ball'
 createBall space ballPicture discInfo pos velocity = do
   ballBody <- createDisc space discInfo pos velocity
-  pure $ Ball' { .. }
-
-
-
-
-
+  pure $ Ball' {..}
 
 handleEvent :: Assets -> Event -> World -> IO World
 handleEvent _ (EventMotion mousePos@(mX, mY)) world@World {slingshot = (ball@Slingshot {slingshotGrabbed = Grabbed})} =
@@ -101,23 +82,28 @@ handleEvent _ (EventMotion mousePos@(mX, mY)) world@World {slingshot = (ball@Sli
     pure world {slingshot = ball {slingshotPosition = newPos}}
 handleEvent _ (EventKey (Char 'q') Down _ _) _ = exitSuccess
 handleEvent
-  Assets{..}
+  Assets {..}
   (EventKey (MouseButton LeftButton) Up _ _)
-  world@World {space, slingshot = (ball@Slingshot {slingshotPosition = sPos@(sX, sY), slingshotGrabbed = Grabbed}), thrownBalls} = do
+  world@World
+    { space,
+      slingshot = (ball@Slingshot {slingshotPosition = sPos@(sX, sY), slingshotGrabbed = Grabbed}),
+      thrownBalls
+    } = do
     let d = distance ballInitPos sPos
         initVelocityNorm = maxInitVelocity * d / maxGrabDist
         (bX, bY) = ballInitPos
         v =
-            ( initVelocityNorm * (bX - sX) / d
-            , initVelocityNorm * (bY - sY) / d
-            )
+          ( initVelocityNorm * (bX - sX) / d,
+            initVelocityNorm * (bY - sY) / d
+          )
 
-    let discInfo = DiscInfo
-             { discRadius = ballRadius
-             , discElasticity = 0.9
-             , discFriction = 0
-             , discMass = 5
-             }
+    let discInfo =
+          DiscInfo
+            { discRadius = ballRadius,
+              discElasticity = 0.9,
+              discFriction = 0,
+              discMass = 5
+            }
     newBall <- createBall space lambdaBall discInfo (sX, sY) v
     pure $
       world
@@ -141,8 +127,6 @@ handleEvent
       -- print $ show $ world'
       pure world'
 handleEvent _ _ world = pure world
-
-
 
 grabCircle :: Radius -> Pos -> Pos -> Bool
 grabCircle radius circlePos clickPos = radius >= distance circlePos clickPos
@@ -180,15 +164,10 @@ advanceSim space collisionQueue advance tic world = do
 
 -- renderBody :: Picture -> Body -> IO Picture
 
-
-
-
 (leftGroundA, leftGroundB) =
   ( Vect leftBankAX leftBankAY,
     Vect leftBankBX leftBankBY
   )
-
-
 
 (rightGroundA, rightGroundB) =
   ( Vect rightBankAX rightBankAY,
@@ -199,42 +178,45 @@ logX, logY :: Floating a => a
 logX = 0
 logY = -145
 
-
 createLog :: Space -> Picture -> BoxInfo Float -> Pos -> IO Log
 createLog space pic boxInfo pos = do
   logBody <- createBox space boxInfo pos
-  pure $ Log { logPicture = pic 
-             , logBody = logBody
-             }
-
+  pure $
+    Log
+      { logPicture = pic,
+        logBody = logBody
+      }
 
 createWorld :: Assets -> Space -> IO World
-createWorld assets@Assets{woodenLog, wood} space = do
+createWorld assets@Assets {woodenLog, wood} space = do
   -- Ground
   _ <- createGround space
 
   -- Log
-  let logInfo = BoxInfo { boxMass = 0.5 
-                        , boxSize = (700, 200)
-                        , boxFriction = 0.5 
-                        , boxElasticity = 0.8
-                        }
+  let logInfo =
+        BoxInfo
+          { boxMass = 0.5,
+            boxSize = (700, 200),
+            boxFriction = 0.5,
+            boxElasticity = 0.8
+          }
 
   let logPos = (0, -145)
 
-  logObj <- createLog space woodenLog logInfo logPos 
+  logObj <- createLog space woodenLog logInfo logPos
 
   -- Blocks
-  let boxInfo = BoxInfo
-        { boxMass = 0.5
-        , boxFriction = 0.5
-        , boxElasticity = 0.8
-        , boxSize = (25,250)
-        }
+  let boxInfo =
+        BoxInfo
+          { boxMass = 0.5,
+            boxFriction = 0.5,
+            boxElasticity = 0.8,
+            boxSize = (25, 250)
+          }
       blocks =
-        [ ( boxInfo,  (250, -25), 0 ),
-          ( boxInfo, (350, -25), 0 ),
-          ( boxInfo, (25, 250), 3.1415 / 2 )
+        [ (boxInfo, (250, -25), 0),
+          (boxInfo, (350, -25), 0),
+          (boxInfo, (25, 250), 3.1415 / 2)
         ]
 
   blocks <- forM blocks $ \(box, pos, _) -> do
@@ -244,17 +226,13 @@ createWorld assets@Assets{woodenLog, wood} space = do
   -- ballBody <- createBall space ballRadius (Vect (-100) 300) (Vect 200 0)
   enemy <- createEnemy space ballRadius (Vect 300 400) (Vect 0 0)
 
-  
-
   -- logImg <- lambda <- loadBMP "imgs/lambda.bmp"
 
   -- World
   pure $ World space blocks initBall logObj [] [enemy]
 
-
 createGround :: Space -> IO ()
 createGround space = do
-  
   -- Create Space body
   spaceBody <- get $ spaceStaticBody space
 
@@ -305,11 +283,3 @@ createEnemy space radius initPos initVelocity = do
   spaceAddShape space enemyShape
 
   pure enemyBody
-
-createCallBacks :: Space -> TQueue EnemyCollision -> IO ()
-createCallBacks space collisionQueue = do
-  callback <- mkCallback (enemyCollisionCallback collisionQueue)
-  colHandlerPtr <- spaceAddWildcardHandler' space EnemyCT
-  modifyCollisionHandler colHandlerPtr $ \colHandler -> pure $ colHandler {chPostSolveFunc = callback}
-
-  pure ()
