677bbfa1a9b9e08c3d73aed890de70ec251cdc99
Gaël Deest <gael.deest@tweag.io>
2021-06-20 11:37:18 +0200

    New beginning

---
+++ app/EventHandling.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
+module EventHandling where
+
+import Assets
+import Chiphunk.Low
+import Collisions
+import Constants
+import Data.Function ((&))
+import Graphics.Gloss.Interface.IO.Game
+    ( Key(MouseButton, Char),
+      KeyState(Down, Up),
+      MouseButton(LeftButton),
+      Event(EventKey, EventMotion) )
+import Physics
+import System.Exit
+import World
+
+handleEvent :: Assets -> Event -> World -> IO World
+-- Quit key event
+handleEvent _ (EventKey (Char 'q') Down _ _) _ = exitSuccess
+handleEvent _ _ world = pure world
+
+handleCollision :: Space -> World -> Collision -> IO World
+handleCollision _ world _ = pure world
---
+++ shell.nix
@@ -0,0 +1,33 @@
+#let nixos = fetchTarball { 
+#  url = "https://releases.nixos.org/nixos/20.09/nixos-20.09.3505.12d9950bf47/nixexprs.tar.xz";
+#  sha256 = "0fsl8bsdb8i536pfs4wrp0826h5l84xqlwx32sbz66jg4ykqp9lr";
+#}; in
+
+with (import <nixpkgs> {});
+
+let
+  ghc = haskell.packages.ghc884.ghcWithPackages (_: []);
+in
+
+stdenv.mkDerivation {
+  name = "build-shell";
+  buildInputs = [
+    cabal-install
+    libGL
+    libGL.dev
+    libGLU
+    glxinfo
+    git
+    gmp.dev
+    libffi.dev
+    freeglut
+    zlib
+    haskell.compiler.ghc8104
+    stack
+  ];
+  shellHook = with pkgs; ''
+    export PATH="$PATH:$PWD/bin"
+  '';
+  # I wish we could do without this ugly hack.
+  LD_LIBRARY_PATH = "${libGL}/lib:${mesa_glu}/lib:${freeglut}/lib:${gmp}/lib:${libffi}/lib";
+}
